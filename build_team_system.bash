a805f8f4be2387f3984c4ef04a756b8bbdaea5e4
Siwei Feng <ustcsiwei88@gmail.com>
2020-04-18 19:43:07 -0400

    it seems it needs to build the whole system again

--- build_team_system.bash
+++ build_team_system.bash
@@ -1,7 +1,7 @@
-#!/usr/bin/env bash
+!/usr/bin/env bash
 . /opt/ros/${ROS_DISTRO}/setup.bash
 cd ~
 git clone https://github.com/ustcsiwei88/RuBot2020.git rubot
 cd rubot
-# git submodule update --init
-catkin_make install
+git submodule update --init
+catkin build
