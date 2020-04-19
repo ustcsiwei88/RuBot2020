9163962d06de22655cc9661f58c0d984b40a9fac
Siwei Feng <ustcsiwei88@gmail.com>
2020-04-18 20:21:46 -0400

    use template script

--- build_team_system.bash
+++ build_team_system.bash
@@ -1,7 +1,25 @@
-!/usr/bin/env bash
+#!/usr/bin/env bash
+
+YELLOW='\033[0;33m'
+GREEN='\033[0;32m'
+NOCOLOR='\033[0m'
+
+# Prepare ROS
+echo -e "${YELLOW}---Sourcing ROS${NOCOLOR}"
 . /opt/ros/${ROS_DISTRO}/setup.bash
-cd ~
-git clone https://github.com/ustcsiwei88/RuBot2020.git rubot
-cd rubot
-git submodule update --init
-catkin build
+echo -e "${YELLOW}---Sourcing ARIAC${NOCOLOR}"
+. /home/ariac-user/ariac_ws/devel/setup.bash
+
+
+# Install the necessary dependencies for getting the team's source code
+# Note: there is no need to use `sudo`.
+# echo -e "${YELLOW}---Installing necessary dependencies${NOCOLOR}"
+# apt-get update
+# apt-get install -y wget unzip
+
+
+# Create a catkin workspace
+echo -e "${YELLOW}---Creating workspace${NOCOLOR}"
+cd /root
+git clone https://github.com/ustcsiwei88/RuBot2020 rubot
+catkin_make
